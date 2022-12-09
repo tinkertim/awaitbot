b56424f8f2c8c26ec1a04596f9f5ec794c10f5b7
Tim Post <tinkertim@gmail.com>
2022-12-09 10:54:41 -0500

    Clean up module code

--- mod.ts
+++ mod.ts
@@ -1,29 +1,17 @@
-
-import { createClient } from './deps.ts'
 import { GatewayIntents } from './deps.ts'
+import { log } from './deps.ts'
 import { AwaitBotClient } from './src/AwaitBotClient.ts'
-
-console.log('Awaitbot starting:')
-
-console.log('Initializing database connection ...')
-const dbUrl = Deno.env.get("DB_CONN_URL")
-const dbAnonPublicKey = Deno.env.get("DB_PUBLIC_ANON_KEY")
-if (dbUrl === undefined || dbAnonPublicKey === undefined) {
-  console.log('You must supply the Supabase DB_PUBLIC_ANON_KEY and DB_CONN_URL in the environment.')
-  Deno.exit()
-}
-const db = await createClient(dbUrl, dbAnonPublicKey)
-console.log('Database successfully connected.')
+import { sql } from './db.ts'
 
 const botToken = Deno.env.get("DISCORD_BOT_TOKEN")
 if (botToken === undefined) {
-  console.log('Fatal: No token provided (DISCORD_BOT_TOKEN must be in the process environment).')
+  log.critical('Fatal: No token provided (DISCORD_BOT_TOKEN must be in the process environment).')
   Deno.exit()
 }
-console.log('Initializing bot client ...')
+log.info('Initializing bot client ...')
 const bot = new AwaitBotClient()
 await bot.connect(botToken, [
   GatewayIntents.GUILDS,
 ])
 
-console.log('Bot initialized. Listening ...')
+log.info('Bot initialized.')
