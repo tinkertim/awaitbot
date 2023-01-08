b468f69048d8fea566d60faf9ea3ff29797a1c37
Tim Post <tinkertim@gmail.com>
2023-01-08 13:18:09 -0500

    This is why I need to set up pre-commit

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/harmony@v2.7.0/mod.ts";
 export * as log from "https://deno.land/std@0.167.0/log/mod.ts";
-export * from 'https://esm.sh/@supabase/supabase-js@2';
+export * from "https://esm.sh/@supabase/supabase-js@2";
--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,10 +1,10 @@
-export type Json = 
+export type Json =
   | string
   | number
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 // export default sql;
 
@@ -13,50 +13,50 @@ export interface Database {
     Tables: {
       social_event_1: {
         Row: {
-          id: number
-          user_id: string
-          promise: string
-          email: string | null
-          submission: string | null
-          inserted: string
-          updated: string
-          fulfilled: boolean | null
-          deleted: string | null
-        }
+          id: number;
+          user_id: string;
+          promise: string;
+          email: string | null;
+          submission: string | null;
+          inserted: string;
+          updated: string;
+          fulfilled: boolean | null;
+          deleted: string | null;
+        };
         Insert: {
-          id?: number
-          user_id: string
-          username: string
-          promise: string
-          email?: string | null
-          submission?: string | null
-          inserted?: string
-          updated?: string,
-          fulfilled?: boolean | null
-          deleted?: string | null
-        }
+          id?: number;
+          user_id: string;
+          username: string;
+          promise: string;
+          email?: string | null;
+          submission?: string | null;
+          inserted?: string;
+          updated?: string;
+          fulfilled?: boolean | null;
+          deleted?: string | null;
+        };
         Update: {
-          id?: number
-          user_id?: string
-          username?: string
-          promise?: string
-          email?: string | null
-          submission?: string | null
-          inserted?: string
-          updated?: string,
-          fulfilled?: boolean | null
-          deleted?: string | null
-        }
-      }
-    }
+          id?: number;
+          user_id?: string;
+          username?: string;
+          promise?: string;
+          email?: string | null;
+          submission?: string | null;
+          inserted?: string;
+          updated?: string;
+          fulfilled?: boolean | null;
+          deleted?: string | null;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
